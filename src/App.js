f5988d28cfe26000f32560d2b120c191c6715ac4
Raul Tinelli Salles <raul.tinelli@gmail.com>
2019-03-19 16:13:55 -0300

    reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import store from './store'
+
+import TodoList from './TodoList'
 
 const App = () => (
-  <div className="App">
-    <h1>Hello World</h1>
-  </div>
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
 );
 
 export default App;
