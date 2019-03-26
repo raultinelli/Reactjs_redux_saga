58676fd704612552ddfe4b48301bcffe4a2fa404
Raul Tinelli Salles <raul.tinelli@gmail.com>
2019-03-26 17:56:07 -0300

    implementação do Saga com loading

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -9,13 +9,16 @@ import * as FavoriteActions from '../../store/actions/favorites'
 class Main extends Component {
 
     static propTypes = {
-        addFavorite: PropTypes.func.isRequired,
-        favorites: PropTypes.arrayOf(PropTypes.shape({
-            id: PropTypes.number,
-            name: PropTypes.string,
-            description: PropTypes.string,
-            url: PropTypes.string
-        })).isRequired
+        addFavoriteRequest: PropTypes.func.isRequired,
+        favorites: PropTypes.shape({
+            loading: PropTypes.bool,
+            data: PropTypes.arrayOf(PropTypes.shape({
+                id: PropTypes.number,
+                name: PropTypes.string,
+                description: PropTypes.string,
+                url: PropTypes.string
+            })),
+        }).isRequired
     }
 
     state = {
@@ -25,7 +28,9 @@ class Main extends Component {
     handleAddRepository = (event) => {
         event.preventDefault()
 
-        this.props.addFavorite()
+        this.props.addFavoriteRequest(this.state.repositoryInput)
+
+        this.setState({ repositoryInput: '' })
     }
 
     render () {
@@ -35,13 +40,15 @@ class Main extends Component {
                     <input 
                         placeholder ="usuário/repositório"
                         value={this.state.repositoryInput}
-                        onChange={ e => this.setState({ repositoryInput: e.target.value })}
+                        onChange={e => this.setState({ repositoryInput: e.target.value })}
                     />
                     <button type="submit">Adicinonar</button>
+
+                    { this.props.favorites.loading && <span> Carregando...</span> }
                 </form>
 
                 <ul>
-                    { this.props.favorites.map(favorite => (
+                    { this.props.favorites.data.map(favorite => (
                         <li key={favorite.id}>
                             <p>
                                 <strong>{favorite.name}</strong> ({favorite.description})
--- src/store/reducers/favorites.js
+++ src/store/reducers/favorites.js
@@ -1,14 +1,14 @@
-const INITIAL_STATE = [];
+const INITIAL_STATE = {
+    loading: false,
+    data: []
+};
 
 export default function favorites (state = INITIAL_STATE, action) {
     switch (action.type) {
-        case 'ADD_FAVORITE':
-            return [ ...state, {
-                id: Math.random(),
-                name: 'facenook/react',
-                description: 'blablabla',
-                url: 'http://github.com/facebook/react'
-            }]
+        case 'ADD_FAVORITE_REQUEST':
+            return { ...state, loading: true }
+        case 'ADD_FAVORITE_SUCCESS':
+            return { ...state, loading: false, data: [ ...state.data, action.payload.data ]}
             default: 
                 return state
     }
